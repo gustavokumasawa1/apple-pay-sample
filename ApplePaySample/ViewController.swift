26af8771eecdc09290b587d59f165fee33a9acd9
Gustavo Kumasawa <gustavo.kumasawa@provider-it.com.br>
2023-12-28 16:55:12 -0300

    Initial Commit

---
+++ ApplePaySample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApplePaySample
+//
+//  Created by Virtual Machine on 28/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
